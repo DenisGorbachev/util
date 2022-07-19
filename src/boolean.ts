816e6922dd8888f120a6b5560aec474062376354
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-07-19 13:10:09 +0700

    Fix Coliquidity project

--- src/boolean.ts
+++ src/boolean.ts
@@ -1,3 +1,7 @@
 export function isTrue(value: boolean) {
   return value === true
 }
+
+export function bool2num(value: boolean) {
+  return value ? 1 : 0
+}
--- src/error.ts
+++ src/error.ts
@@ -1,3 +1,19 @@
+import { stringify } from './JSON'
+
+export class CustomError extends Error {
+  constructor(message: string, public props: object) {
+    super(message + ' ' + stringify(props))
+  }
+
+  toJSON() {
+    return JSON.stringify(this, Object.getOwnPropertyNames(this))
+  }
+
+  toString() {
+    return this.toJSON()
+  }
+}
+
 export class CompositeError extends Error {
   constructor(public errors: Error[]) {
     super('Multiple errors occurred: \n\n' + errors.map(e => `- ${e}\n`))
