bbce37a61ccc91d9e97b7b4d55a66260e2768635
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-09-15 14:58:26 +0700

    WIP

--- src/i18next.ts
+++ src/i18next.ts
@@ -3,3 +3,7 @@ import { TFunction } from 'i18next'
 export function withNamespace(t: TFunction, ns: string) {
   return (key: string) => t(key, { ns })
 }
+
+export function withNamespaces(t: TFunction, nses: string[]) {
+  return nses.map(ns => withNamespace(t, ns))
+}
