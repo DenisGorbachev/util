3f73eb2841a756fe985cdb97643d758f9945e820
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-09-18 13:36:08 +0700

    index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+export * from 'src/axios.d.ts'
+export * from 'src/execa.d.ts'
+export * from 'src/chai-bignumber.d.ts'
+export * from 'src/luxon-business-days.d.ts'
--- index.ts
+++ index.ts
@@ -1,1 +1,3 @@
+// TODO: Reexport every file, so that imports would look like "zenbox-util/env" instead of "zenbox-util/src/env"
+
 export * from './src/env'
