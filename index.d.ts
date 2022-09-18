48237c1b96f5fa19e75863e72ad42422ec592b02
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-09-18 13:50:35 +0700

    index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,9 @@
 export * from 'src/axios.d.ts'
-export * from 'src/execa.d.ts'
-export * from 'src/chai-bignumber.d.ts'
-export * from 'src/luxon-business-days.d.ts'
+
+export type * from 'src/execa.d.ts'
+
+export type * from 'src/chai-bignumber.d.ts'
+
+export type * from 'src/luxon-business-days.d.ts'
+
+export type * from 'src/tag.d.ts'
