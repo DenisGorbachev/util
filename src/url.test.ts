f7201828664f5e7f728d86006947906f32ea5205
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-09-20 11:54:03 +0700

    url

--- src/url.test.ts
+++ src/url.test.ts
@@ -1,9 +1,15 @@
 import { expect } from './chai'
 import { test } from '@jest/globals'
-import { getTopLevelDomainFromHostname } from './url'
+import { getTopLevelDomain, getTopLevelDomainFromHostname } from './url'
 
-test('getTopLevelDomain', async function () {
-  expect(getTopLevelDomainFromHostname('sub.sub.example.com')).to.equal('example.com')
-  expect(getTopLevelDomainFromHostname('sub.example.com')).to.equal('example.com')
+test(getTopLevelDomain.name, async function () {
+  expect(getTopLevelDomain('https://example.com')).to.equal('example.com')
+  expect(getTopLevelDomain('https://www.example.com')).to.equal('example.com')
+  expect(getTopLevelDomain('https://sub.www.example.com')).to.equal('example.com')
+})
+
+test(getTopLevelDomainFromHostname.name, async function () {
   expect(getTopLevelDomainFromHostname('example.com')).to.equal('example.com')
+  expect(getTopLevelDomainFromHostname('sub.example.com')).to.equal('example.com')
+  expect(getTopLevelDomainFromHostname('sub.sub.example.com')).to.equal('example.com')
 })
--- src/url.ts
+++ src/url.ts
@@ -1,7 +1,14 @@
+/**
+ * "getDomain", not "getHostname", because "domain" is more common
+ */
+export function getDomain(url: string) {
+  return new URL(url).hostname
+}
+
 export function getTopLevelDomainFromHostname(hostname: string) {
   return hostname.split('.').slice(-2).join('.')
 }
 
-export function getDomainFromUrl(url: string) {
-  return new URL(url).hostname
+export function getTopLevelDomain(url: string) {
+  return getTopLevelDomainFromHostname(getDomain(url))
 }
