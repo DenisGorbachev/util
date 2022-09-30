fc7ad069f2c7cf474e771c2d3c992faf194a099c
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-09-30 21:36:11 +0700

    adjust

--- src/string.ts
+++ src/string.ts
@@ -6,13 +6,17 @@ export interface Stringable {
  * Removes spaces at the beginning of each line (allows to indent multiline strings in code, then remove this indent via `nail()` call)
  * @see ./string.test.ts
  */
-export function nail(str: string): string {
+export function nail(str: string) {
   const spacesAtStart = str.match(/^\n(\s+)/)
   if (spacesAtStart) {
     return str.replace(new RegExp(`^[^\\S\r\n]{0,${spacesAtStart[1].length}}`, 'gm'), '')
   } else {
     return str
   }
+}
+
+export function adjust(str: string) {
+  return nail(str).trim()
 }
 
 export function indent(count: number, str: string) {
