3099eb65b80097fab1324eb3032c8752bb68697d
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-10-03 13:07:46 +0700

    getEnumValues

--- src/typescript.ts
+++ src/typescript.ts
@@ -9,3 +9,13 @@ export function toEnum<K extends string>(...args: K[]): {[P in K]: P} {
 }
 
 export type Ordinal = number | string | Date
+
+type EnumObject = {[key: string]: number | string};
+
+type EnumObjectEnum<E extends EnumObject> = E extends {[key: string]: infer ET | string} ? ET : never;
+
+export function getEnumValues<E extends EnumObject>(enumObject: E): EnumObjectEnum<E>[] {
+  return Object.keys(enumObject)
+    .filter(key => Number.isNaN(Number(key)))
+    .map(key => enumObject[key] as EnumObjectEnum<E>)
+}
