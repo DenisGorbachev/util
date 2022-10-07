7b388a68d9601ef11b535d798080b7f8c40b6456
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-10-07 19:00:23 +0700

    ensure

--- src/ensure.ts
+++ src/ensure.ts
@@ -1,29 +1,25 @@
 import { identity } from 'lodash-es'
 import { Box, BoxP, unbox, unboxP } from './box'
 
-export const notFound = new Error('Can\'t find object in collection')
-
-export const get_notFound = <Obj>(filter: (object: Obj) => boolean) => new Error('Can\'t find an object in a collection using filter: ' + filter.toString())
-
-export function ensure<Obj, Res>(object: Obj | null | undefined, errorBox: Box<Error> = notFound) {
-  if (object === null || object === undefined) throw unbox(errorBox)
+export function ensure<Obj, Err>(object: Obj | null | undefined, error?: Box<Err>) {
+  if (object === null || object === undefined) throw unbox(error ?? notFound)
   return object
 }
 
-export async function ensureP<Obj, Res>(object: Obj | null | undefined, errorBox: BoxP<Error> = notFound) {
-  if (object === null || object === undefined) throw await unboxP(errorBox)
+export async function ensureP<Obj, Err>(object: Obj | null | undefined, error: BoxP<Err>) {
+  if (object === null || object === undefined) throw await unboxP(error ?? notFound)
   return object
 }
 
-export function ensureFind<Obj, Err>(collection: Obj[], filter: (object: Obj) => boolean, errorBox: Box<Error> = get_notFound(filter)) {
+export function ensureFind<Obj, Err>(collection: Obj[], filter: (object: Obj) => boolean, error?: Box<Err>) {
   const object = collection.find(filter)
-  if (object === null || object === undefined) throw unbox(errorBox)
+  if (object === null || object === undefined) throw unbox(error ?? get_notFound(filter))
   return object
 }
 
-export async function ensureFindP<Obj, Err>(collection: Obj[], filter: (object: Obj) => boolean, errorBox: BoxP<Error> = get_notFound(filter)) {
+export async function ensureFindP<Obj, Err>(collection: Obj[], filter: (object: Obj) => boolean, error?: BoxP<Err>) {
   const object = collection.find(filter)
-  if (object === null || object === undefined) throw await unboxP(errorBox)
+  if (object === null || object === undefined) throw await unboxP(error ?? get_notFound(filter))
   return object
 }
 
@@ -31,10 +27,11 @@ export function ensureMapGet<Key, Value>(map: Map<Key, Value>, key: Key) {
   return ensure(map.get(key), new Error(`Can't find key "${key}" in map ${map}`))
 }
 
-export function checkAll<Obj, Res>(objects: Array<Obj | null | undefined>) {
-  if (!objects.every(identity)) {
-    throw new Error(`Some objects are falsy: \n\n${JSON.stringify(objects)}`)
-  } else {
-    return objects
-  }
+export function ensureEvery<Obj, Err>(objects: Array<Obj | null | undefined>, error?: BoxP<Err>) {
+  if (!objects.every(identity)) throw unbox(error ?? new Error(`Some objects are falsy: \n\n${JSON.stringify(objects)}`))
+  return objects
 }
+
+export const notFound = new Error('Can\'t find object in collection')
+
+export const get_notFound = <Obj>(filter: (object: Obj) => boolean) => new Error('Can\'t find an object in a collection using filter: ' + filter.toString())
