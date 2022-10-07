269ebbb4f5fb7e67473488ccfb3ab93607f4a8b7
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-10-07 18:13:40 +0700

    Box

--- src/ensure.ts
+++ src/ensure.ts
@@ -1,33 +1,30 @@
 import { identity } from 'lodash-es'
-import { Box, unbox } from './box'
+import { Box, BoxP, unbox, unboxP } from './box'
 
 export const notFound = new Error('Can\'t find object in collection')
 
 export const get_notFound = <Obj>(filter: (object: Obj) => boolean) => new Error('Can\'t find an object in a collection using filter: ' + filter.toString())
 
 export function ensure<Obj, Res>(object: Obj | null | undefined, errorBox: Box<Error> = notFound) {
-  if (object === null || object === undefined) {
-    throw unbox(errorBox)
-  } else {
-    return object
-  }
+  if (object === null || object === undefined) throw unbox(errorBox)
+  return object
 }
 
-export function ensureStatic<Obj, Err>(object: Obj | null | undefined, errorBox: Box<Error> = notFound) {
-  if (object === null || object === undefined) {
-    throw unbox(errorBox)
-  } else {
-    return object
-  }
+export async function ensureP<Obj, Res>(object: Obj | null | undefined, errorBox: BoxP<Error> = notFound) {
+  if (object === null || object === undefined) throw await unboxP(errorBox)
+  return object
 }
 
 export function ensureFind<Obj, Err>(collection: Obj[], filter: (object: Obj) => boolean, errorBox: Box<Error> = get_notFound(filter)) {
   const object = collection.find(filter)
-  if (object === null || object === undefined) {
-    throw unbox(errorBox)
-  } else {
-    return object
-  }
+  if (object === null || object === undefined) throw unbox(errorBox)
+  return object
+}
+
+export async function ensureFindP<Obj, Err>(collection: Obj[], filter: (object: Obj) => boolean, errorBox: BoxP<Error> = get_notFound(filter)) {
+  const object = collection.find(filter)
+  if (object === null || object === undefined) throw await unboxP(errorBox)
+  return object
 }
 
 export function ensureMapGet<Key, Value>(map: Map<Key, Value>, key: Key) {
