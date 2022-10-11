3382b18882d3a779dd5025bfcc8e5f55d5a838bb
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-10-11 20:28:53 +0700

    publish

--- publish.sh
+++ publish.sh
@@ -12,3 +12,5 @@ cp $DIR/src/*.d.ts "$DIST"
 cp "$DIR/dotenv.cjs" "$DIST"
 cp "$DIR/package.json" "$DIST"
 yarn publish --non-interactive "$DIST"
+sleep 1 # workaround for race condition: sometimes the NPM still returns the old package version right after publish
+[ -f "$DIR/post-publish.local.sh" ] && . "$DIR/post-publish.local.sh"
