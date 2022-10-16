3701215d39b71aba4f82cfb7f3cbba3db0c5dc0a
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-10-16 17:48:10 +0700

    getExclusiveGetterP

--- src/data.ts
+++ src/data.ts
@@ -1,42 +1,42 @@
 import { Mutex } from 'async-mutex'
 import { isDefined } from './typescript'
 
-export type Getter<Obj> = () => Promise<Obj>
+export type GetterP<This, Val> = (this: This) => Promise<Val>
 
 /**
  * Note: this function does not accept any arguments, because otherwise only the first result would be cached; subsequent invocations would return the same result for different arguments
  */
-export function getGetter<This, Result>(fetcher: (this: This) => Promise<Result>) {
-  let result: Result | undefined = undefined
+export function getExclusiveGetterP<This, Val>(getter: GetterP<This, Val>) {
+  let value: Val | undefined = undefined
   const mutex = new Mutex()
   return async function (this: This) {
     const release = await mutex.acquire()
-    if (isDefined(result)) {
+    if (isDefined(value)) {
       release()
-      return result
+      return value
     } else {
       try {
-        result = await fetcher.apply(this)
+        value = await getter.apply(this)
       } finally {
         // release even if the fetcher throws an error
         release()
       }
-      return result
+      return value
     }
   }
 }
 
-export function getGetterWithThisRedirect<Proj, Result>(fetcher: (project: Proj) => Promise<Result>) {
+export function getExclusiveGetterPWithThisRedirect<Ctx, Result>(runner: (context: Ctx) => Promise<Result>) {
   let result: Result | undefined = undefined
   const mutex = new Mutex()
-  return async function (this: Proj) {
+  return async function (this: Ctx) {
     const release = await mutex.acquire()
     if (isDefined(result)) {
       release()
       return result
     } else {
       try {
-        result = await fetcher(this)
+        result = await runner(this)
       } finally {
         // release even if the fetcher throws an error
         release()
