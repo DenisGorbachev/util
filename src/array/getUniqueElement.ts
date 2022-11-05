b74e6a419a06844630955cc54993c2529dd08806
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-11-05 12:42:01 +0700

    Extract from zenbox

--- src/array/getUniqueElement.ts
+++ src/array/getUniqueElement.ts
@@ -1,7 +1,5 @@
 import { isEqual } from 'lodash-es'
 
-export type NonEmptyArray<T> = [T, ...T[]]
-
 export function getUniqueElement<T>(elements: T[]) {
   const first = elements[0]
   const everyElementIsSame = elements.every((element) => isEqual(element, first))
@@ -10,9 +8,4 @@ export function getUniqueElement<T>(elements: T[]) {
   } else {
     throw new Error('Some elements are different')
   }
-}
-
-export function toEvenLength<T>(array: T[]) {
-  const isEven = array.length % 2 == 0
-  return isEven ? array : array.slice(0, -1)
 }
