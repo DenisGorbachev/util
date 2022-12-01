3b3134adb12be528a3c6f61353f7cedb64f6ed85
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-12-01 20:30:35 +0700

    expectEqualResults

--- src/fast-check/commands/GenericCommand.ts
+++ src/fast-check/commands/GenericCommand.ts
@@ -1,6 +1,5 @@
 import { AsyncCommand } from 'fast-check'
-import { ImplementationError } from '../../todo'
-import { expect } from '../../chai'
+import { expectEqualResults } from '../../chai/expectEqualResults'
 import { stringify } from '../../JSON'
 
 // eslint-disable-next-line @typescript-eslint/ban-types
@@ -10,24 +9,7 @@ export abstract class GenericCommand<Model extends object, Real, Result> impleme
   }
 
   async expectEqualResults(modelTxPromise: Promise<Result>, realTxPromise: Promise<Result>) {
-    const [modelResult, realResult] = await Promise.allSettled([modelTxPromise, realTxPromise])
-    try {
-      expect(modelResult.status).to.equal(realResult.status)
-      if (modelResult.status === 'fulfilled' && realResult.status === 'fulfilled') {
-        expect(modelResult.value).to.deep.equal(realResult.value)
-      } else if (modelResult.status === 'rejected' && realResult.status === 'rejected') {
-        expect(modelResult.reason.toString()).to.equal(realResult.reason.toString())
-        if (modelResult.reason instanceof ImplementationError || realResult.reason instanceof ImplementationError) {
-          throw new Error('Unexpected ImplementationError')
-        }
-      }
-    } catch (e) {
-      if (e instanceof Error) {
-        if (modelResult.status === 'rejected') e.message += '\n\nModel ' + modelResult.reason.stack
-        if (realResult.status === 'rejected') e.message += '\n\nReal ' + realResult.reason.stack
-      }
-      throw e
-    }
+    return expectEqualResults('Model', 'Real')(modelTxPromise, realTxPromise)
   }
 
   abstract check(model: Readonly<Model>): Promise<boolean>
