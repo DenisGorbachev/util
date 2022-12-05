78bb516f3f901665799958a22980fe072fa8b496
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-12-05 11:39:39 +0700

    startsWith

--- src/lodash.ts
+++ src/lodash.ts
@@ -7,17 +7,13 @@ export type MapperP<U, V> = (obj: U) => Promise<V>
 
 export type IsEqual<U> = (a: U) => (b: U) => boolean
 
-export function isEqualBy<U, V>(a: U, b: U, mapper: Mapper<U, V>) {
-  return isEqual(mapper(a), mapper(b))
-}
+export const isEqualC = <U>(a: U) => (b: U) => isEqual(a, b)
 
-export function equal<U>(a: U) {
-  return (b: U) => isEqual(a, b)
-}
+export const isEqualSC = <U>(a: U) => (b: U) => a === b
 
-export function equalBy<U, V>(a: U, mapper: Mapper<U, V>) {
-  return (b: U) => isEqualBy(a, b, mapper)
-}
+export const isEqualBy = <U, V>(a: U, b: U, mapper: Mapper<U, V>) => isEqual(mapper(a), mapper(b))
+
+export const isEqualByC = <U, V>(mapper: Mapper<U, V>) => (a: U) => (b: U) => isEqualBy(a, b, mapper)
 
 export function isSubsetOf<T>(set: T[], subset: T[]) {
   return difference(set, subset).length === 0
